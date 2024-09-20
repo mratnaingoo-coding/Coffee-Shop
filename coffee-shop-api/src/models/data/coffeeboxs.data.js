5bdd5ebd5db91ea7aa31c2bae6155427a69f7cc4
Rikzil <142309328+MratRikzil@users.noreply.github.com>
2024-09-21 00:07:43 +0630

    Nav & Layout with Get Coffee [Complete]

--- coffee-shop-api/src/models/data/coffeeboxs.data.js
+++ coffee-shop-api/src/models/data/coffeeboxs.data.js
@@ -2,22 +2,22 @@ const coffeeboxs = [
     {
         id: 1,
         itemName: "Black-Coffee",
-        price: "2,500 kyats"
+        price: "2,500 "
     },
     {
         id: 2,
         itemName: "Cappuccino",
-        price: "3,500 kyats"
+        price: "3,500 "
     },
     {
         id: 3,
         itemName: "Latte",
-        price: "4,000 kyats"
+        price: "4,000 "
     },
     {
         id: 4,
         itemName: "Espresso",
-        price: "5,000 kyats"
+        price: "5,000 "
     },
 ]
 export default coffeeboxs;
